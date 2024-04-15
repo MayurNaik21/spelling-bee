93787ff7d9a3db3828ee7e5c3238972073c61069
MayurNaik21 <mayurnaik2104@gmail.com>
2024-04-16 01:25:01 +0530

    add words.csv and skip word button functionality

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,40 +1,72 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Col, Divider, Flex, Input, Row, Tag } from "antd";
-import { CheckCircleOutlined } from "@ant-design/icons";
+import {
+  CheckCircleOutlined,
+  CloseCircleOutlined,
+  SoundOutlined,
+} from "@ant-design/icons";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Papa from "papaparse";
 
 const Game = () => {
-  const initialWords = [
-    "Bougainvillea",
-    "Quarantine",
-    "Insouciance",
-    "Programming",
-    "Neighbor",
-    "Mountain",
-    "Amazing",
-    "Chocolate",
-    "Success",
-  ];
   const [inputWord, setInputWord] = useState("");
   const [randomWord, setRandomWord] = useState("");
-  const [wordsArray, setWordsArray] = useState(initialWords);
+  const [wordsArray, setWordsArray] = useState([]);
   const [correctWords, setCorrectWords] = useState([]);
+  const [skippedWords, setSkippedWords] = useState([]);
+  const [wordIsGuessed, setWordIsGuessed] = useState(false);
+
+  const readCSVFile = async () => {
+    const filePath = "src/words/words.csv";
+
+    fetch(filePath)
+      .then((response) => response.text())
+      .then((data) => {
+        const parsedData = Papa.parse(data, {
+          delimiter: ",",
+          header: false,
+        });
+        const words = parsedData.data
+          .flat()
+          .map((word) => word.trim())
+          .filter((word) => word !== "");
+        setWordsArray(words);
+      })
+      .catch((error) => {
+        console.error("Error fetching and parsing CSV file:", error);
+      });
+  };
+
+  useEffect(() => {
+    readCSVFile();
+    setWordIsGuessed(true);
+  }, []);
 
   const handleChange = (e) => {
     setInputWord(e.target.value);
   };
 
-  const getRandomWord = () => {
-    if (wordsArray.length === 0) {
-      alert("All words have been guessed! Game over.");
-      return;
-    }
+  const getNewRandomWord = () => {
     const index = Math.floor(Math.random() * wordsArray.length);
     const newRandomWord = wordsArray[index];
+    return newRandomWord;
+  };
 
-    setRandomWord(newRandomWord);
-    speakRandomword(newRandomWord);
+  const getRandomWord = () => {
+    if (wordsArray.length === 0) {
+      toast.success("All words have been guessed! Game over.");
+      return;
+    }
+
+    if (!wordIsGuessed) {
+      speakRandomword(randomWord);
+    } else {
+      setWordIsGuessed(false);
+      const newRandomWord = getNewRandomWord();
+      setRandomWord(newRandomWord);
+      speakRandomword(newRandomWord);
+    }
   };
 
   const speakRandomword = (randomWord) => {
@@ -58,6 +90,7 @@ const Game = () => {
       );
       setWordsArray(updatedWordsArray);
       setInputWord("");
+      setWordIsGuessed(true);
       return;
     }
     toast.error("Incorrect! Try again.");
@@ -66,9 +99,22 @@ const Game = () => {
 
   const restartGame = () => {
     setInputWord("");
-    setWordsArray(initialWords);
+    setWordsArray([]);
     setRandomWord("");
     setCorrectWords([]);
+    readCSVFile();
+  };
+
+  const handleSkip = () => {
+    if (randomWord !== "") {
+      setSkippedWords([...skippedWords, randomWord]);
+      const updatedWordsArray = wordsArray.filter(
+        (word) => word.toLowerCase() !== randomWord.toLowerCase()
+      );
+      setWordsArray(updatedWordsArray);
+      setInputWord("");
+      setWordIsGuessed(true);
+    }
   };
 
   return (
@@ -82,7 +128,10 @@ const Game = () => {
               style={{ color: "#3a3637", fontSize: "2rem", padding: "2rem" }}
             >
               <h2>Start Guessing</h2>
-              <Button onClick={getRandomWord}>Speak Word</Button>
+              <Button onClick={getRandomWord}>
+                {" "}
+                <SoundOutlined /> Speak Word
+              </Button>
             </div>
           </Col>
         </Row>
@@ -99,10 +148,13 @@ const Game = () => {
                   required
                 />
                 <Button type="primary" onClick={checkRandomWord}>
-                  Check
+                  Check Word
                 </Button>
 
                 <Button onClick={restartGame}>Restart Game</Button>
+                <Button type="primary" danger onClick={handleSkip}>
+                  Skip This Word
+                </Button>
               </Flex>
             </div>
           </Col>
@@ -113,9 +165,10 @@ const Game = () => {
                 backgroundColor: "#f5f5f5",
                 padding: "1rem",
                 borderRadius: "10px",
+                overflow: "auto",
               }}
             >
-              <Divider>Correctly Guessed Words</Divider>
+              <Divider>Result</Divider>
               <Flex gap="4px 0" wrap="wrap">
                 {correctWords.map((word, index) => (
                   <Tag
@@ -123,6 +176,12 @@ const Game = () => {
                     key={index}
                     color="success"
                   >
+                    {word}
+                  </Tag>
+                ))}
+
+                {skippedWords.map((word, index) => (
+                  <Tag icon={<CloseCircleOutlined />} key={index} color="error">
                     {word}
                   </Tag>
                 ))}
